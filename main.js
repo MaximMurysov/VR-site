50bd148493ff94b0c55841a3eace5ebdf8b3e0ba
Maxim Murysov <murisov98@mail.ru>
2025-08-16 23:51:49 +0300

    Добавил анимации на кнопки и всплывающие окна

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
-const hiddenNoode = document.querySelector(".hidden-text");
-let hideTimeout;
-const buttonNode = document.getElementById("test-js");
+const menuLinks = document.querySelectorAll(".header-menu");
+const hideTimeouts = {};
 
-buttonNode.addEventListener("click", (e) => {
-  e.preventDefault();
-  const text = document.querySelector(".hidden-text");
-  text.classList.toggle("active");
-  if (hideTimeOout) clearTimeout(hideTimeOout);
-  hiddenNoode.classList.add("active");
-  hideTimeOout = setTimeout(() => {
-    hiddenNoode.classList.remove("active");
-  }, 3000);
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const text = this.querySelector(".hidden-text");
+    document.querySelectorAll(".hidden-text.active").forEach((activeText) => {
+      if (activeText !== text) {
+        activeText.classList.remove("active");
+        if (hideTimeouts[activeText.id]) {
+          clearTimeout(hideTimeouts[activeText.id]);
+        }
+      }
+    });
+    text.classList.toggle("active");
+    if (hideTimeouts[text.id]) {
+      clearTimeout(hideTimeouts[text.id]);
+    }
+    if (text.classList.contains("active")) {
+      hideTimeouts[text.id] = setTimeout(() => {
+        text.classList.remove("active");
+      }, 3000);
+    }
+    if (this.classList.contains("with-arrow")) {
+      this.classList.toggle("active");
+    }
+  });
 });
+//Кнопка hero_btn
+const heroBtnNode = document.querySelector('.hero__btn');
+const hiddenHeroNode = document.querySelector('.hidden_hero_btn')
+
+heroBtnNode.addEventListener('click', () =>{
+ hiddenHeroNode.classList.add('active')
+ setTimeout(() => {
+  hiddenHeroNode.classList.remove('active')
+ }, 3000);
+})
+
+//play
+const  playNode =  document.querySelector('.play')
+const hiddenImgNode = document.querySelector('.hidden_image_text')
+
+playNode.addEventListener('click', () =>{
+  hiddenImgNode.classList.add('active')
+  setTimeout(() => {
+    hiddenImgNode.classList.remove('active')
+  }, 3000);
+})
+
+
